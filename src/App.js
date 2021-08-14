dcc54d23417ecbe706737c97c8a50b9477fa6f3f
NagaiKoki <kokinagai10+1@gmail.com>
2021-08-14 11:48:53 +0900

    enable profiling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Profiler } from "react";
+import Item from "./Item";
 
-function App() {
+let num = 10000;
+const array = new Array(num).fill(null).map((_, i) => {
+  return { id: i, name: `todo: ${i}` };
+});
+
+const logTimes = (id, phase, actualTime) => {
+  console.log(`${id}'s ${phase} phase:`);
+  console.log(`Actual time: ${actualTime}`);
+};
+
+export const App = () => {
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profiler id="react-pf" onRender={logTimes}>
+      <div>
+        <h1 style={{ marginBottom: "20px" }}>Performance measurement</h1>
+        <input onChange={(e) => setText(e.target.value)} />
+        <span style={{ marginBottom: 30 }}>text: {text}</span>
+        <ul style={{ display: "flex", flexWrap: "wrap" }}>
+          {array.map((item) => {
+            return (
+              <li style={{ margin: 10 }} key={item.id}>
+                <Item name={item.name} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Profiler>
   );
-}
-
-export default App;
+};
---
+++ src/Item.jsx
@@ -0,0 +1,20 @@
+import React, { memo } from "react";
+
+const Item = ({ name }) => {
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        padding: "1px 10px",
+        background: "red",
+        listStyle: "none",
+      }}
+    >
+      <strong>{name}</strong>
+    </div>
+  );
+};
+
+// export default memo(Item);
+export default Item;
